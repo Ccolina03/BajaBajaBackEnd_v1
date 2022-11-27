78c54312e50874712194db579e32532d8e40aefb
Ccolina03 <colinaperu2003@gmail.com>
2022-11-27 00:37:06 -0700

    creator id correct wiring up

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -84,7 +84,7 @@ const login = async (req, res, next) => {
       return next(error);
     }
     
-    res.json({message:"Logged IN"})
+    res.json({message:"Logged IN", user: existingUser.toObject({getters:true})})
 };
 
 exports.getUsers= getUsers;
